1d123361da7b6959cb9aebaa0a7236a935aefd55
NierNeonzz22 <Reinbrendan22@gmail.com>
2025-10-19 19:05:33 +0800

    Main Menu
    Main menu addition

--- README.md
+++
@@ -1,2 +0,0 @@
-# The Ashen Veil: RELICON Game
-
---
+++ scripts/langit_moving.gd
@@ -0,0 +1,18 @@
+# Animation for Langit movement
+
+extends Sprite2D
+
+@export var scroll_speed: float = 25.0  # pixels per second
+var start_x: float
+var end_x: float
+
+func _ready():
+    start_x = position.x
+    end_x = texture.get_width() - get_viewport_rect().size.x
+
+func _process(delta):
+    position.x += scroll_speed * delta
+
+    # When the background reaches the end, loop back
+    if position.x > end_x or position.x < start_x:
+        scroll_speed = -scroll_speed  # reverse direction
