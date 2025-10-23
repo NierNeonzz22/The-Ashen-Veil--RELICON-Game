988895a0dbbcbe9c261336dcdac4500516914e63
nocheleydon <leydonnoche04@gmail.com>
2025-10-23 22:03:19 +0800

    save

--- scripts/langit_moving.gd
+++ scripts/langit_moving.gd
@@ -7,12 +7,12 @@ var start_x: float
 var end_x: float
 
 func _ready():
-    start_x = position.x
-    end_x = texture.get_width() - get_viewport_rect().size.x
+	start_x = position.x
+	end_x = texture.get_width() - get_viewport_rect().size.x
 
 func _process(delta):
-    position.x += scroll_speed * delta
+	position.x += scroll_speed * delta
 
-    # When the background reaches the end, loop back
-    if position.x > end_x or position.x < start_x:
-        scroll_speed = -scroll_speed  # reverse direction
+	# When the background reaches the end, loop back
+	if position.x > end_x or position.x < start_x:
+		scroll_speed = -scroll_speed  # reverse direction
