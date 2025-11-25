b5c23c7ec10076a02055fa262cf17bb18cf9fb2a
IunoBall <darkspectrum96@gmail.com>
2025-11-25 18:08:54 +0800

    Add files via upload

---
+++ scripts/button_1(3).gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://option_1_result_(scene_3_third_choice).tscn")
---
+++ scripts/option_3.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://option_1_&_3_result (scene 3 first choice).tscn")
+	
